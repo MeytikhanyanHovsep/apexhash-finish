8d2f6eb351d7ce2ca84b9f4c9aa53fb5c2d17383
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2026-01-09 14:10:52 +0400

    finished

--- script.js
+++ script.js
@@ -63,3 +63,25 @@ function handleForm(event) {
     document.getElementById("name").value = ""
 }
 form.addEventListener('submit', handleForm);
+
+const input = document.getElementById('phone');
+
+input.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, ''); // Удаляем всё, кроме цифр
+    let formattedValue = '';
+
+    if (value.length > 0) {
+        formattedValue += value.substring(0, 3);
+    }
+    if (value.length > 3) {
+        formattedValue += ' ' + value.substring(3, 6);
+    }
+    if (value.length > 6) {
+        formattedValue += '-' + value.substring(6, 8);
+    }
+    if (value.length > 8) {
+        formattedValue += '-' + value.substring(8, 10);
+    }
+
+    e.target.value = formattedValue;
+});
